0448d248f0e4d053dc5013f321071ee342879f1a
Dino Ronconi <dronconi@practia.global>
2021-08-02 16:01:08 -0300

    Priemra version compuesta con los archivos que tenia de antes

---
+++ primer_archivo.py
@@ -0,0 +1,26 @@
+vo# Usar context managers para apertura de archivos o conecciones a bases de datos, lo cual nos evita tener que 
+# preocuparnos por cerrar las conecciones después de usar los recursos
+
+# Caso típico:
+f = open('archivo.txt', 'r')
+contenido = f.read()
+# si no necesito volver a usar el archivo, ya lo cierro y libero recursos
+f.close()
+
+palabras = contenido.split(' ')
+cantidad_palabras = len(palabras)
+
+print('Vuelvo a modificar esta leyenda: ',cantidad_palabras)
+
+# Versión usando context manager
+with open('archivo.txt', 'r') as f:
+    contenido = f.read()
+# NOTA: ver que no hace falta cerrar el archivo en forma explícita
+
+palabras = contenido.split(' ')
+cantidad_palabras = len(palabras)
+
+print('Cantidad de palabras: '+chr(cantidad_palabras))
+
+print('Cantidad de palabras mas 5: '+chr(cantidad_palabras+5))
+
